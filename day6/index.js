945d97b2be9add401168ed87cbcabf2a3281d162
Jean-Michel Reupsch <reupsch@brunsdigital.de>
2025-12-10 13:05:46 +0100

    day6 done

--- day6/index.js
+++ day6/index.js
@@ -1,35 +1,50 @@
-const input = './testInput';
+const input = './input';
 const file = Bun.file(input);
 
 const text = await file.text();
 
 const lines = text.split('\n');
-const operations = lines.map((line) => {
-	return line.split(' ').filter((value) => value !== '');
-});
+const problems = [];
+
+let currentProblem = [];
+for(let i = 0; i <= lines[0].length; i++) {
+	
+	let rows = [];
+	for(let j = 0; j < lines.length; j++) {
+		if(lines[j][i] == "*" || lines[j][i] == "+") {
+			currentProblem.push(lines[j][i])
+			continue;
+		}
+
+		rows.push(lines[j][i]);
+	}
+
+	const number = rows.filter(v => v !== " ").join("");
+	if(number !== "") currentProblem.unshift(number)
+
+	if(rows.every(v => v == " ")) {
+		problems.push(currentProblem);
+		currentProblem = [];
+		continue;
+	}
+}
+problems.push(currentProblem);
 
 let totalSum = 0;
 
-for (let i = 0; i < operations[0].length; i++) {
-	const operator = operations[operations.length - 1][i];
-
-	const numbers = [];
-	for (let j = 0; j < operations.length - 1; j++) {
-		for(let k = 0; k < operations[j][i].length; k++) {
-			if(!numbers[k]) numbers[k] = "";
-			numbers[k] += operations[j][i][k];
-		}
-	}
-	console.log(numbers)
+for (let i = 0; i < problems.length; i++) {
+	let currentProblem = problems[i];
+	const operator = currentProblem[currentProblem.length - 1];
+	currentProblem.splice(currentProblem.length - 1, 1);
 
 	let sum = 0;
 	switch (operator) {
 		case '*':
 			if(sum == 0) sum = 1;
-			for (let number of numbers) sum *= parseInt(number);
+			for (let number of currentProblem) sum *= parseInt(number);
 			break;
 		case '+':
-			for (let number of numbers) sum += parseInt(number);
+			for (let number of currentProblem) sum += parseInt(number);
 			break;
 	}
 
